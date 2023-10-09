0b566c4c606796a0fb3c393639bb5ca5b1d48938
shunze.chen <qianlonwork@outlook.com>
2023-10-09 21:13:18 +0800

    add axes

--- src/main/main.js
+++ src/main/main.js
@@ -26,9 +26,25 @@ document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
-function render() {
+const axesHelper = new three.AxesHelper(5);
+scene.add(axesHelper);
+
+function render(timer) {
+  // 1s 转 PI
+  cube.rotation.x = (Math.PI * timer) / 1000;
   // 使用渲染器通过相机渲染场景
   renderer.render(scene, camera);
   requestAnimationFrame(render);
 }
 render();
+
+window.addEventListener("resize", function () {
+  // 摄像头
+  camera.aspect = window.innerWidth / window.innerHeight;
+  // 更新投影矩阵
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(this.window.innerWidth, this.window.innerHeight);
+  // devicePixelRatio 返回物理像素分辨率/CSS 像素分辨率，即告诉浏览器使用多少屏幕实际像素绘制单个 CSS 像素
+  renderer.setPixelRatio(this.window.devicePixelRatio);
+});
