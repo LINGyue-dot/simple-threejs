f01208d3a4300fd642676796ea08986ff1d939ad
shunze.chen <qianlonwork@outlook.com>
2023-10-10 22:49:29 +0800

    材质分辨率算法

--- src/main/main.js
+++ src/main/main.js
@@ -4,26 +4,23 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 const scene = new three.Scene();
 
 const camera = new three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 0, 10);
+camera.position.set(1, 1, 10);
 scene.add(camera);
 
-const cubeGeometry = new three.BufferGeometry();
-const cubeMaterial = new three.MeshBasicMaterial({ color: 0xffffff });
-// dprint-ignore
-const position = new Float32Array([
-    -1,-1,1, // 
-    1,-1,1,
-    1,1,1,
-    1,1,1,
-    -1,1,1,
-    -1,-1,1
-])
+const textureLoader = new three.TextureLoader();
+const minecraftTexture = textureLoader.load(
+  "https://typora-1300781048.cos.ap-beijing.myqcloud.com/img/202310102229521.png"
+);
 
-const bufferAttribute = new three.BufferAttribute(position, 3);
-cubeGeometry.setAttribute("position", bufferAttribute);
+minecraftTexture.magFilter = three.NearestFilter;
+minecraftTexture.minFilter = three.NearestFilter;
+
+const cubeGeometry = new three.BoxGeometry(2, 2, 2);
+const cubeMaterial = new three.MeshBasicMaterial({ color: 0xffffff, map: minecraftTexture });
 
 const cube = new three.Mesh(cubeGeometry, cubeMaterial);
-
+cube.rotateY(Math.PI / 4);
+cube.rotateX(Math.PI / 4);
 scene.add(cube);
 
 const renderer = new three.WebGL1Renderer();
@@ -37,8 +34,6 @@ const axesHelper = new three.AxesHelper(5);
 scene.add(axesHelper);
 
 function render(timer) {
-  // 1s 转 PI
-  cube.rotation.x = (Math.PI * timer) / 1000;
   // 使用渲染器通过相机渲染场景
   renderer.render(scene, camera);
   requestAnimationFrame(render);
